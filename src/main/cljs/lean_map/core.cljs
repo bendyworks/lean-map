2eebdd1cc84e99cb35f2532495336efc003c6bd7
spinningtopsofdoom <dev+spinningtopsofdoom@bendyworks.com>
2015-11-09 15:49:29 -0600

    Added conj to PersistentHashMap

--- src/main/cljs/lean_map/core.cljs
+++ src/main/cljs/lean_map/core.cljs
@@ -197,6 +197,20 @@
   IMeta
   (-meta [coll] meta)
 
+  ICollection
+  (-conj [coll entry]
+    (if (vector? entry)
+      (-assoc coll (-nth entry 0) (-nth entry 1))
+      (loop [ret coll es (seq entry)]
+        (if (nil? es)
+          ret
+          (let [e (first es)]
+            (if (vector? e)
+              (recur (-assoc ret (-nth e 0) (-nth e 1))
+                     (next es))
+              (throw (js/Error. "conj on a map takes map entries or seqables of map entries"))))))))
+
+
   IEmptyableCollection
   (-empty [coll] (-with-meta (.-EMPTY PersistentHashMap) meta))
 

843d06b929f556653af89140170c95f7d111d00f
spinningtopsofdoom <dev+spinningtopsofdoom@bendyworks.com>
2015-11-09 19:09:39 -0600

    WIP for dissoc for PersistentHashMap

--- src/main/cljs/lean_map/core.cljs
+++ src/main/cljs/lean_map/core.cljs
@@ -136,7 +136,54 @@
         (not (zero? (bit-and nodemap bit)))
         (.inode-lookup (node-at arr nodemap bit) (+ shift 5) hash key not-found)
         :else
-        not-found))))
+        not-found)))
+
+  (copy-and-remove-value [indoe e bit]
+    (let [idx (* 2 (bitmap-indexed-node-index datamap bit))
+          len (alength arr)
+          dst (make-array (- len 2))]
+      (array-copy arr 0 dst 0 idx)
+      (array-copy arr (+ idx 2) dst idx (- len idx 2))
+      (BitmapIndexedNode. e (bit-xor datamap bit) nodemap dst)))
+
+  (copy-and-migrate-to-inline [inode e bit node]
+    (let [idx-old (- (alength arr) 1 (bitmap-indexed-node-index nodemap bit))
+          idx-new (* 2 (bitmap-indexed-node-index datamap bit))
+          dst (make-array (inc (alength arr)))]
+      (array-copy arr 0 dst 0 idx-new)
+      (aset dst idx-new (aget node 0))
+      (aset dst idx-new (aget node 1))
+      (array-copy arr idx-new dst (+ idx-new 2) (- idx-old idx-new))
+      (array-copy arr (inc idx-old) dst (+ idx-old 2) (- (alength arr) idx-old 1))
+      (BitmapIndexedNode. e (bit-or datamap bit) (bit-xor nodemap bit) dst)))
+
+  (inode-without [inode wedit shift hash key changed?]
+    (let [bit (bitpos hash shift)]
+      (cond
+        (not (zero? (bit-and datamap bit)))
+        (let [idx (bitmap-indexed-node-index datamap bit)]
+          (if (key-test key (aget arr (* 2 idx)))
+            (do
+              (set! (.-modified changed?) true)
+              (if (and (== 2 (bit-count datamap)) (zero? nodemap))
+               (let [new-datamap (if (zero? shift) (bit-xor datamap bit) (bitpos hash 0))]
+                 (if (zero? idx)
+                   (BitmapIndexedNode. wedit new-datamap 0 (array (aget arr 2) (aget arr 3)))
+                   (BitmapIndexedNode. wedit new-datamap 0 (array (aget arr 0) (aget arr 1)))))
+               (.copy-and-remove-value inode wedit bit)))
+            inode))
+        (not (zero? (bit-and nodemap bit)))
+        (let [sub-node (aget arr (node-at arr nodemap bit))
+              sub-node-new (.inode-without sub-node wedit (+ shift 5) hash key changed?)]
+          (if (.-modified changed?)
+            (if (== 1 (bit-count nodemap))
+              (if (zero? datamap)
+                (.copy-and-migrate-to-inline inode wedit bit sub-node-new)
+                sub-node-new)
+              (.copy-and-set-node inode wedit bit sub-node-new))
+            inode))
+        :else
+        inode))))
 
 (set! (.-EMPTY BitmapIndexedNode) (BitmapIndexedNode. nil 0 0 (make-array 0)))
 
@@ -246,7 +293,16 @@
 
   IEditableCollection
   (-as-transient [coll]
-    (TransientHashMap. (js-obj) root cnt)))
+    (TransientHashMap. (js-obj) root cnt))
+
+  IMap
+  (-dissoc [coll k]
+    (if (nil? root)
+      coll
+      (let [new-root (.inode-without root nil 0 (hash k) k (Box. false false))]
+        (if (identical? new-root root)
+          coll
+          (PersistentHashMap. meta (dec cnt) new-root nil))))))
 
 (set! (.-EMPTY PersistentHashMap) (PersistentHashMap. nil 0 nil empty-unordered-hash))
 
@@ -364,17 +420,45 @@
 (def cem (.-EMPTY cljs.core/PersistentHashMap))
 
 (comment
-  (let [times 50
+  (let [times 100
         hm1 (loop [m lem i 0]
-              (if (< i times)
-                (recur (assoc m (str "key" i) i) (inc i))
-                m))
-        root (.-root hm1)]
-    (println hm1)
-    [ (count hm1)  (seq hm1)]
-    #_[(count hm1)
-       (= (into #{} (vals hm1)) (into #{} (range times)))
-       (= (into #{} (vals hm1)) (into #{} (map #(get hm1 (str "key" %)) (range times))))
-       (= (keys hm1) (map (fn [[k v]] k) hm1))
-       (= (into #{} (keys hm1)) (into #{} (map #(str "key" %) (range times))))])
-  )
+               (if (< i times)
+                 (recur (assoc m (str "key" i) i) (inc i))
+                 m))]
+      (println
+        ["assoc"
+         (count hm1)
+         (= (into #{} (vals hm1)) (into #{} (range times)))
+         (= (into #{} (vals hm1)) (into #{} (map #(get hm1 (str "key" %)) (range times))))
+         (= (keys hm1) (map (fn [[k v]] k) hm1))
+         (= (into #{} (keys hm1)) (into #{} (map #(str "key" %) (range times))))])
+    (let [subchan (/ times 2)
+          dtimes (- times subchan)
+          dhm1 (loop [m hm1 i 0]
+                 (if (< i subchan)
+                   (recur (dissoc m (str "key" (- times i 1))) (inc i))
+                   m))]
+      ["dissoc"
+       (count dhm1)
+       (= (into #{} (vals dhm1)) (into #{} (range dtimes)))
+       (= (into #{} (vals dhm1)) (into #{} (map #(get dhm1 (str "key" %)) (range dtimes))))
+       (= (keys dhm1) (map (fn [[k v]] k) dhm1))
+       (= (into #{} (keys dhm1)) (into #{} (map #(str "key" %) (range dtimes))))]))
+  (do
+    (js/console.profile "CLJS Map")
+    (let [times 10000
+          hm1 (loop [m cem i 0]
+                (if (< i times)
+                  (recur (assoc m (str "key" i) i) (inc i))
+                  m))]
+      [(count hm1)]
+      (js/console.profileEnd)))
+  (do
+    (js/console.profile "Lean Map")
+    (let [times 10000
+         hm1 (loop [m lem i 0]
+               (if (< i times)
+                 (recur (assoc m (str "key" i) i) (inc i))
+                 m))]
+     [(count hm1)]
+     (js/console.profileEnd))))

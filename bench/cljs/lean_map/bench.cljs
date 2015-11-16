8ba7f44d75cd86e5b540f80cf97a5a7849f7f981
spinningtopsofdoom <dev+spinningtopsofdoom@bendyworks.com>
2015-11-16 07:13:23 -0600

    Setup first pass for benchmarks
    - The benchmarks are setup like ClojureScript's (running via JS engines)

--- bench/cljs/lean_map/bench.cljs
+++ bench/cljs/lean_map/bench.cljs
@@ -1,4 +1,9 @@
-(ns cljs.lean-map.bench)
+(ns cljs.lean-map.bench
+  (:require [cljs.lean-map.core :as lean]))
+
+(def println print)
+
+(set! *print-fn* js/print)
 
 (def small-map-size 100)
 (def small-map-sample 2000)
@@ -14,20 +19,24 @@
    medium-map-size medium-map-sample
    large-map-size large-map-sample})
 
+(declare test-keys)
+
 (defn sized-map [m n]
   (loop [m m i 0]
     (if (< i n)
-      (recur (assoc m (str "key" i) i) (inc i))
+      (recur (assoc m (get test-keys i) i) (inc i))
       m)))
 
 (defmulti map-bench
   (fn [benchmark _ _] benchmark))
 
 (defmethod map-bench :assoc [_ m _]
+  (println "Assoc")
   (doseq [[size sample] size->sample]
     (simple-benchmark [] (sized-map m size) sample)))
 
 (defmethod map-bench :dissoc [_ _ {:keys [small-map medium-map large-map]}]
+  (println "Dissoc")
   (let [maps [small-map medium-map large-map]
         samples [small-map-sample medium-map-sample large-map-sample]]
     (doseq [[m sample] (partition 2 (interleave maps samples))]
@@ -35,10 +44,11 @@
        []
        (loop [m m i (count m)]
          (when (> i 0)
-           (recur (dissoc m (str "key" i)) (dec i))))
+           (recur (dissoc m (get test-keys i)) (dec i))))
        sample))))
 
 (defmethod map-bench :dissoc-fail [_ _ {:keys [small-map medium-map large-map]}]
+  (println "Dissoc Fail")
   (let [maps [small-map medium-map large-map]
         samples [small-map-sample medium-map-sample large-map-sample]]
     (doseq [[m sample] (partition 2 (interleave maps samples))]
@@ -46,16 +56,18 @@
         []
         (loop [m m i (count m)]
           (when (> i 0)
-            (recur (dissoc m (str "not-a-key" i)) (dec i))))
+            (recur (dissoc m i) (dec i))))
         sample))))
 
 (defmethod map-bench :hash [_ _ {:keys [small-map medium-map large-map]}]
+  (println "Hash")
   (let [maps [small-map medium-map large-map]
         samples [small-map-sample medium-map-sample large-map-sample]]
     (doseq [[m sample] (partition 2 (interleave maps samples))]
       (simple-benchmark [] (hash-unordered-coll m) sample))))
 
 (defmethod map-bench :equals [_ _ {:keys [small-map medium-map large-map]}]
+  (println "Equals")
   (let [maps [small-map medium-map large-map]
         samples [small-map-sample medium-map-sample large-map-sample]]
     (doseq [[m sample] (partition 2 (interleave maps samples))]
@@ -63,6 +75,7 @@
         (simple-benchmark [] (= m cm) sample)))))
 
 (defmethod map-bench :equals-fail [_ _ {:keys [small-map medium-map large-map]}]
+  (println "Equals fail")
   (let [maps [small-map medium-map large-map]
         samples [small-map-sample medium-map-sample large-map-sample]]
     (doseq [[m sample] (partition 2 (interleave maps samples))]
@@ -70,20 +83,48 @@
         (simple-benchmark [] (= m neq-m) sample)))))
 
 (defmethod map-bench :sequence [_ _ {:keys [small-map medium-map large-map]}]
+  (println "sequence")
   (let [maps [small-map medium-map large-map]
         samples [small-map-sample medium-map-sample large-map-sample]]
     (doseq [[m sample] (partition 2 (interleave maps samples))]
       (simple-benchmark [] (doseq [[k v] m] [v k]) sample))))
 
 (defmethod map-bench :reduce [_ _ {:keys [small-map medium-map large-map]}]
+  (println "Reduce")
   (let [maps [small-map medium-map large-map]
         samples [small-map-sample medium-map-sample large-map-sample]]
     (doseq [[m sample] (partition 2 (interleave maps samples))]
       (simple-benchmark [] (reduce (fn [sum [k v]] (+ 1 sum)) 0 m) sample))))
 
 (defn run-benchmarks [benchmarks empty-map data]
+  (println "Running Benchmarks")
   (doseq [benchmark benchmarks]
     (map-bench benchmark empty-map data)))
+
+(def lem (.-EMPTY PersistentHashMap))
+(def cem (.-EMPTY cljs.core/PersistentHashMap))
+
+(def test-keys (mapv (fn [i]
+                       (let [nk (keyword (str "key" i))]
+                         (hash nk)
+                         nk))
+                     (range large-map-size)))
+
+(println "Current Maps")
+(let [benchmarks  [:assoc :dissoc :dissoc-fail :hash :equals :equals-fail :sequence :reduce]
+      empty-map cem
+      data {:small-map (sized-map empty-map small-map-size)
+            :medium-map (sized-map empty-map medium-map-size)
+            :large-map (sized-map empty-map large-map-size)}]
+  (run-benchmarks benchmarks empty-map data))
+
+(println "Lean Maps")
+(let [benchmarks  [:assoc :dissoc :dissoc-fail :hash :equals :equals-fail :sequence :reduce]
+      empty-map lem
+      data {:small-map (sized-map empty-map small-map-size)
+            :medium-map (sized-map empty-map medium-map-size)
+            :large-map (sized-map empty-map large-map-size)}]
+  (run-benchmarks benchmarks empty-map data))
 
 (comment
   (let [benchmarks  [:assoc :dissoc :dissoc-fail :hash :equals :equals-fail :sequence :reduce]
---
+++ script/bench.clj
@@ -0,0 +1,8 @@
+(require '[cljs.build.api :as b])
+
+(b/build (b/inputs "src/main" "bench")
+         {:asset-path "bench"
+          :output-to "resources/bench/app.js"
+          :output-dir "resources/bench"
+          :optimizations :advanced
+          :verbose true})

bad120a30eccdf88600c6b03e64d6f24504b88ab
spinningtopsofdoom <dev+spinningtopsofdoom@bendyworks.com>
2015-11-18 11:07:21 -0600

    Moved "bench" to "src/bench"

--- script/bench.clj
+++ script/bench.clj
@@ -1,6 +1,6 @@
 (require '[cljs.build.api :as b])
 
-(b/build (b/inputs "src/main" "bench")
+(b/build (b/inputs "src/main" "src/bench")
          {:asset-path "bench"
           :output-to "resources/bench/app.js"
           :output-dir "resources/bench"

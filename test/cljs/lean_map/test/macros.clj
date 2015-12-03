0d00db4b0cb2488b8311acd66e2695bd2422f7e7
spinningtopsofdoom <dev+spinningtopsofdoom@bendyworks.com>
2015-12-03 08:35:37 -0600

    Remove local collection check files

--- test/cljs/lean_map/test/macros.clj
+++
@@ -1,8 +0,0 @@
-(ns cljs.lean-map.test.macros
-  (:require [cljs.lean-map.test.utils :as utils]))
-
-(defmacro reporting-failing-actions [& body]
-  `(let [old-report-fn# ~'cljs.test/report]
-     (binding [cljs.test/report #(do (old-report-fn# %)
-                                     (cljs.lean-map.test.utils/report-failing-actions %))]
-       ~@body)))

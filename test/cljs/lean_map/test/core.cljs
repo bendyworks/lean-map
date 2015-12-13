114f705fe9fe24a3cf2fc4898c654d73c8fe0e34
spinningtopsofdoom <dev+spinningtopsofdoom@bendyworks.com>
2015-12-13 10:57:54 -0600

    Create keys that have hash collision
    Previously lean-map used keys that had no hash collisions. No a datatype is used that hash only a single hashcode

--- test/cljs/lean_map/test/core.cljs
+++ test/cljs/lean_map/test/core.cljs
@@ -5,8 +5,21 @@
     [clojure.test.check.generators :as gen]
     [cljs.lean-map.core :as lean-map.core]))
 
-(def gen-element
+(defrecord BadHashNumber [num])
+
+(extend-protocol IHash
+  BadHashNumber
+  (-hash [_]
+    1))
+
+(def gen-bad-hash
+  (gen/fmap (partial apply ->BadHashNumber) (gen/tuple gen/int)))
+
+(def gen-key
+  (gen/tuple (gen/frequency [[9 gen/int] [1 gen-bad-hash]])))
+
+(def gen-value
   (gen/tuple gen/int))
 
 (deftest assert-lean-map-core-map-like
-  (cc/assert-map-like 100 (.-EMPTY lean-map.core/PersistentHashMap) gen-element gen-element))
+  (cc/assert-map-like 100 (.-EMPTY lean-map.core/PersistentHashMap) gen-key gen-value))
